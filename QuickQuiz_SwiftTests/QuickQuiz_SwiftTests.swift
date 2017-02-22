a44cb71fda79f8f9f2b3a5669f0a69cd554fe279
Admin <admin@mac-admin.local>
2017-02-22 20:33:08 +0200

    Initial Commit

---
+++ QuickQuiz_SwiftTests/QuickQuiz_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuickQuiz_SwiftTests.swift
+//  QuickQuiz_SwiftTests
+//
+//  Created by Admin on 22.02.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import XCTest
+@testable import QuickQuiz_Swift
+
+class QuickQuiz_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
